d238daec4dadb79bf67fd0f166cfc336d061e093
Frumbasjt <richardgankema@gmail.com>
2015-12-01 10:11:35 +0100

    Updated Views

--- cfsl/src/main/java/nl/utwente/cs/fmt/cfsl/App.java
+++ cfsl/src/main/java/nl/utwente/cs/fmt/cfsl/App.java
@@ -19,6 +19,7 @@ public class App extends Application {
         
         stage.setTitle("CFSL+");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
